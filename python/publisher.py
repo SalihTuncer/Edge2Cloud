fd52ff8efeb25cc662155dae9b973f68421dfaa1
Salih Tuncer <Salih.Tuncr98@web.de>
2021-11-12 15:29:17 +0100

    the health monitoring is now part of the publisher.py it sends now JSON-objects to the broker chart.js added to dependencies

--- python/publisher.py
+++ python/publisher.py
@@ -1,15 +1,59 @@
 # publisher
 import paho.mqtt.client as mqtt
-from random import randrange, uniform
 import time
+import psutil
+import json 
 
-client = mqtt.Client('publisher')
-client.connect('localhost', 1883)
-#client.connect('mqtt.eclipseprojects.io')
+'''
+Scale bytes to its proper format
+source: https://stackoverflow.com/questions/1094841/get-human-readable-version-of-file-size
+'''
+def size_of(num, suffix='B'):
+    for unit in ['', 'Ki', 'Mi', 'Gi', 'Ti', 'Pi', 'Ei', 'Zi']:
+        if abs(num) < 1024.0:
+            return f'{num:3.1f}{unit}{suffix}'
+        num /= 1024.0
+    return f'{num:.1f}Yi{suffix}'
 
+def cpu_informations():
+    return {
+		'cpu_usage': psutil.cpu_percent(interval=0.025)
+	}
 
-while True:
-    randNumber = uniform(20.0, 21.0)
-    client.publish('health_monitoring', randNumber)
-    print(f'Just published {randNumber:10.2f} to Topic health_monitoring')
-    time.sleep(1)
+def memory_informations():
+	mem = psutil.virtual_memory()
+	return {
+		'total_mem': size_of(mem.total),
+		'used_mem': size_of(mem.used)
+	}
+
+def disk_informations():
+	io_counters = psutil.disk_io_counters()
+	return {
+		'disk_read': size_of(io_counters.read_count),
+		'disk_write': size_of(io_counters.write_count)
+	}
+
+'''
+def temperature_informations():
+	return {
+		'Temperature:' psutil.sensors_temperatures()
+	}
+'''
+
+if __name__ == '__main__':
+
+    client = mqtt.Client('publisher')
+    client.connect('localhost', 1883)
+
+    interval = 1
+
+    while True:
+        cpu = cpu_informations()
+        mem = memory_informations()
+        disk = disk_informations()
+        stats = json.dumps([cpu, mem, disk])
+        
+        client.publish('health_monitoring', stats)
+        print(f'health_monitoring: {stats}')
+        time.sleep(interval)
